04a0638243f8e7adc77ea193ca7215ad0e5901ff
zctp011 <32876346+zctp011@users.noreply.github.com>
2017-11-27 19:39:44 +0000

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,48 +1,56 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+
 
 public class FileReader {
 
-	public static Scanner fileScanner;
-	public static File quizFile;
-
-	public static void main(String[] args) {
-
-		String question = readFiles("d:/mini_quiz.csv");
-
-		if (question == null) {
-			System.out.println("No question found.");
-		} else {
-			System.out.println(question);
-		}
-	}
-
-	public static String readFiles(String file) {
-		try {
-			quizFile = new File(file);
-			
-			String[] questions = new String[findTotalQuestions()];			
-
-			fileScanner = new Scanner(quizFile);
-
-			for (int index = 0; index < questions.length; index++) {
-				questions[index] = fileScanner.nextLine();
-			}
-			
-			return questions[new Random().nextInt(questions.length)];
-		}
-		catch (FileNotFoundException fileNotFoundException) {
-			fileNotFoundException.printStackTrace();
-			System.out.println("File mini_quiz.csv was not found.");
-			System.out.println("or couldn't be opened");
-			System.exit(0);
-		}catch(Exception exception){
-			exception.printStackTrace();
-		}
-		return null;
-	}
-
-	public static int findTotalQuestions() throws Exception{
-		fileScanner = new Scanner(quizFile);
+    public static void main(String[] args) {
+        
+        String[] Question = readFiles("mini_quiz.csv");
+      
+        for(int index=0; index < Question.length; index++)
+        {
+        System.out.println(Question[index]);
+        }
+        
+    }
+   //Method that return an array of 10 randomly selected questions from a file//
+   public static String[] readFiles(String file) {
+    try{
+     //Counts the number of lines in the file//
+      File f = new File(file);
+      Scanner s = new Scanner(f);
+      int counter = 0;
+      while (s.hasNextLine()){
+        counter++;
+        s.nextLine();
+      }
+      //Creates an array for the Questions in the file//
+      String[] AllQuestions = new String[counter];
+      Scanner s1 = new Scanner(f);
+      
+      for(int index=0; i < AllQuestions.length; index++){
+        AllQuestions[index] = s1.nextLine();
+      
+      }
+      //Creates an array for 10 randomly selected Questions from the file//
+      String[] SelectedQuestions = new String[10];
+       for(int index=0; index < 10; index++)
+      SelectedQuestions[i] = AllQuestions[new Random().nextInt(AllQuestions.length)];
+      return SelectedQuestions;
+      
+    }
+      
+    catch (FileNotFoundException e)
+        {
+          System.out.println("File Sentence.txt was not found.");
+          System.out.println("or couldn't be opened");
+          System.exit(0);
+        }
+   return null;
+  }
+  
+}
